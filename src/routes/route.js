fcab8a9920799ba2b0456ad022287c2f8fa0f887
soumya <97720811+coderSidd03@users.noreply.github.com>
2022-08-08 22:44:32 +0530

    assignment-get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,43 +2,69 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
-
-    res.send('My second ever api!')
+router.get('/movies', function (req, res) {
+    let movies = ["Tenet", "The Avengers", "Iron Man", "Captain America: Civil War", "Avengers: End Game"]
+    res.send(movies)
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+router.get('/movies/:indexNumber', function (req, res) {
 
-router.get('/student-details/:name/:age', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
+    let movies = ["Tenet", "The Avengers", "Iron Man", "Captain America: Civil War", "Avengers: End Game"]
     let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    //(req.params) is stores an object like key value pairs
-    // here (req.params)
-    // http://localhost:3000/student-details/sid/27
-    // storing the dynamic name input iside an object with :name and :age as key and the given name and given age as value
-    let studentName = requestParams.name
-    let studentAge = requestParams.age
-    console.log('Name of the student is ', studentName)
-    
-    let studentDetails = studentName + " " + studentAge
-    res.send(studentDetails)
+    let number = requestParams.indexNumber;
+    (number > movies.length) ?
+        res.send('>> use valid index between < 1 to ' + movies.length + ' > to get data \t >> check full collection http://localhost:3000/movies') : res.send(movies[number - 1])
 });
+
+
+router.get('/films', function (req, res) {
+
+    let films = [
+        {
+            "id": 1,
+            "name": "The Matrix: Resurrection"
+        }, 
+        {
+            "id": 2,
+            "name": "Thor Ragnarok"
+        }, 
+        {
+            "id": 3,
+            "name": "Dr. Strange: Madness in the multiverse"
+        }, 
+        {
+            "id": 4,
+            "name": "Spiderman: No Way Home"
+        }
+    ]
+    res.send(films)
+});
+
+router.get('/films/:filmId', function (req, res) {
+
+    let films = [
+        {
+            "id": 1,
+            "name": "The Matrix: Resurrection"
+        }, 
+        {
+            "id": 2,
+            "name": "Thor Ragnarok"
+        }, 
+        {
+            "id": 3,
+            "name": "Dr. Strange: Madness in the multiverse"
+        }, 
+        {
+            "id": 4,
+            "name": "Spiderman: No Way Home"
+        }
+    ]
+
+    let requestParams = req.params;
+    let id = requestParams.filmId;
+    (id > films.length) ? res.send('>> No movie exists with this id \n check films : http://localhost:3000/films') : res.send(films[id-1]);
+});
+
 
 module.exports = router;
