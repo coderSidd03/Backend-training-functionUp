bc152d4808397c6802ed6911e75f9b8241347176
soumya <97720811+coderSidd03@users.noreply.github.com>
2022-08-09 22:29:32 +0530

    nothing

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,130 +1,49 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
 
 const router = express.Router();
 
-// router.get('/test-me', function (req, res) {
-//     myHelper.printDate()
-//     myHelper.getCurrentMonth()
-//     myHelper.getCohortData()
-//     let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-//     console.log('The first element received from underscope function is '+firstElement)
-//     res.send('My first ever api!')
-// });
 
-// router.get("/movies/:indexNumber", function(req, res){
-//     const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-//     console.log(req.params.indexNumber)
-//     let movieIndex = req.params.indexNumber
-//     //check index value. less than 0 or greater than array (length - 1) are not valid
-//     if(movieIndex<0 || movieIndex>=movies.length) {
-//         //if the index is invalid send an error message
-//         return res.send('The index value is not correct, Please check the it')
-//     }
+//  Q1. -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
 
-//     //if the index was valid send the movie at that index in response
-//     let requiredMovie = movies[movieIndex]
-//     res.send(requiredMovie)
-// })
+router.get('/s1', (req, res) => {
 
-router.get('/shoes', (req, res) => {
+    let arr = [1, 2, 3, 5, 6, 7];
+    let total = 0;
 
-    let queryParams = req.query
-    let brand = queryParams.brand
-    let size = queryParams.size
-    let color = queryParams.color
-    let pattern = queryParams.pattern
-    console.log(">> brand  -> ", brand)
-    console.log(">> size  -> ", size)
-    console.log(">> color  -> ", color)
-    console.log(">> pattern  ->", pattern)
-    res.send('dummy response')
-})
+    for (let i in arr) {
+        total += arr[i];
+    }
 
+    let lastElem = arr.pop();
+    let allElemSum = (lastElem * (lastElem + 1)) / 2;
+    let missingElem = allElemSum - total;
 
-// uses query params
-// in query param we basically filters out data from database
-// for this below code we can take value with state, gender, district from the url
-
-// localhost:3000/candidates?gender=female&state=west bengal&district=hooghly
-
-//gender=female&state=west bengal&district=hooghly      >>these are like key & value
-//as we can see above through that hitpoint with filters we can getcertaing outputs with a lot more precesion 
-//and storing req.query.gender to a variable gender here and same for all so to use this variable inside code  
-
-
-router.get('/candidates', function (req, res) {
-    console.log('Query paramters for this request are ' + JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is ' + state)
-    console.log('Gender is ' + gender)
-    console.log('District is ' + district)
-    let candidates = ['Akash', 'Suman']
-    res.send(candidates)
+    res.send(">> the missing element inside array is : ", missingElem)
 })
 
 
 
-// use path param
-// here we take value from hit points in url directly in mentioned variable name
-// here  :canidatesName
-router.get('/candidates/:canidatesName', function (req, res) {
-    console.log('The request objects is ' + JSON.stringify(req.params))
-    console.log('Candidates name is ' + req.params.canidatesName)
-    res.send('Done')
+
+//  Q2. -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+
+router.get('/s2', (req, res) => {
+
+    // let arr = [33, 34, 35, 37, 38];
+    // let length = arr.length
+    // let total = 0;
+
+    // for (let i in arr) {
+    //     total += arr[i];
+    // }
+
+    // let frstElem = arr[0];
+    // let lastElem = arr.pop()
+    // let allElemSum = (length + 1) * (frstElem + lastElem) / 2;
+    // let missingElem = allElemSum - total;
+
+    res.send(">> the missing element inside array is : ")
 })
 
-// router.get("/films", function (req, res) {
-//     const films = [{
-//         "id": 1,
-//         "name": "The Shining"
-//     }, {
-//         "id": 2,
-//         "name": "Incendies"
-//     }, {
-//         "id": 3,
-//         "name": "Rang de Basanti"
-//     }, {
-//         "id": 4,
-//         "name": "Finding Nemo"
-//     }]
-//     //send all the films
-//     res.send(films)
-// })
 
-// router.get("/films/:filmId", function (req, res) {
-//     const films = [{
-//         "id": 1,
-//         "name": "The Shining"
-//     }, {
-//         "id": 2,
-//         "name": "Incendies"
-//     }, {
-//         "id": 3,
-//         "name": "Rang de Basanti"
-//     }, {
-//         "id": 4,
-//         "name": "Finding Nemo"
-//     }]
-
-//     let filmId = req.params.filmId
-
-//     //iterate all the films
-//     //search for a film whose id matches with the id recevied in request
-//     for (let i = 0; i < films.length; i++) {
-//         let film = films[i]
-//         if (film.id == filmId) {
-//             //if there is a match return the response from here
-//             return res.send(film)
-//         }
-//     }
-
-//     //if there is no match give an error response
-//     res.send("The film id doesn't match any movie")
-// })
 
 module.exports = router;
-// adding this comment for no reason
