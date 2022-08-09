1b61d071cc224f7d0a103a9328dfc7c400f0d8c2
soumya <97720811+coderSidd03@users.noreply.github.com>
2022-08-09 22:52:47 +0530

    assignment-sol-missing-array-element -09-08-2022

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,46 +4,39 @@ const router = express.Router();
 
 
 //  Q1. -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
-
-router.get('/s1', (req, res) => {
-
+router.get('/sol1', (req, res) => {
     let arr = [1, 2, 3, 5, 6, 7];
     let total = 0;
 
-    for (let i in arr) {
-        total += arr[i];
+    for (let i=0; i<arr.length; i++) {
+        total = total + (arr[i]);
     }
 
     let lastElem = arr.pop();
     let allElemSum = (lastElem * (lastElem + 1)) / 2;
     let missingElem = allElemSum - total;
 
-    res.send(">> the missing element inside array is : ", missingElem)
+    res.send(">> the missing element inside array is : " + missingElem);
 })
-
 
 
 
 //  Q2. -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+router.get('/sol2', (req, res) => {
 
-router.get('/s2', (req, res) => {
+    let arr = [33, 34, 35, 37, 38];
+    let length = arr.length
+    let total = 0;
 
-    // let arr = [33, 34, 35, 37, 38];
-    // let length = arr.length
-    // let total = 0;
+    for (let i in arr) {
+        total += arr[i];
+    }
 
-    // for (let i in arr) {
-    //     total += arr[i];
-    // }
+    let frstElem = arr[0];
+    let lastElem = arr.pop()
+    let allElemSum = (length + 1) * (frstElem + lastElem) / 2;
+    let missingElem = allElemSum - total;
 
-    // let frstElem = arr[0];
-    // let lastElem = arr.pop()
-    // let allElemSum = (length + 1) * (frstElem + lastElem) / 2;
-    // let missingElem = allElemSum - total;
-
-    res.send(">> the missing element inside array is : ")
+    res.send(">> the missing element inside array is : " + missingElem);
 })
-
-
-
 module.exports = router;
