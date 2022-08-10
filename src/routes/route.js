bb8439642748046e511714bef84a9b8f91f5c752
soumya <97720811+coderSidd03@users.noreply.github.com>
2022-08-10 23:02:57 +0530

    assignment-player-problem-votingage

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,56 +1,81 @@
+const { query } = require('express');
 const express = require('express');
 
 const router = express.Router();
 
-let players =
-    [
-        {
-            "name": "manish",
-            "dob": "1/1/1995",
-            "gender": "male",
-            "city": "jalandhar",
-            "sports": [
-                "swimming"
-            ]
-        },
-        {
-            "name": "gopal",
-            "dob": "1/09/1995",
-            "gender": "male",
-            "city": "delhi",
-            "sports": [
-                "soccer"
-            ],
-        },
-        {
-            "name": "lokesh",
-            "dob": "1/1/1990",
-            "gender": "male",
-            "city": "mumbai",
-            "sports": [
-                "soccer"
-            ],
-        },
-    ]
 
 
 
+// ASSIGNMENT :- 1 <post-api> <player-data>
 
 router.post('/players', (req, res) => {
 
     let name = req.body.name;
+    let player = players.find((element) => element.name == name)
 
-    for (var i in players) {
-        let player = players[i];
-        if (name == player.name) {
-            res.send(">> Player is already existed")
-        }
-    }
-
+    (player) ? res.send(">> Player is already existed") 
+    :
     players.push(req.body)
-    res.send('>> player added')
-    console.log(players)
+    res.send(players);
+    
 });
+
+
+
+//problem : 2 <votingAge filter problem>
+
+let persons = [
+    {
+        name: "PK",
+        age: 10,
+        votingStatus: false
+    },
+    {
+        name: "SK",
+        age: 20,
+        votingStatus: false
+    },
+    {
+        name: "AA",
+        age: 70,
+        votingStatus: false
+    },
+    {
+        name: "SC",
+        age: 5,
+        votingStatus: false
+    },
+    {
+        name: "HO",
+        age: 40,
+        votingStatus: false
+    }
+]
+
+router.post('/persons', (req, res) => {
+    let votingAge = req.query.age;
+    let ageResult = persons.filter((elem) => elem.age > votingAge)
+    for (let i in ageResult) { ageResult[i].votingStatus = true; }
+    res.send(ageResult)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
