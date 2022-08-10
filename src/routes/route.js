4aff9e7f5837639811aa1db7e3b0c94f6329d4a1
Fear Reaper <97720811+coderSidd03@users.noreply.github.com>
2022-08-10 23:43:28 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,6 +8,31 @@ const router = express.Router();
 
 // ASSIGNMENT :- 1 <post-api> <player-data>
 
+let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": ["swimming"]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": ["soccer"],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": ["soccer"],
+        },
+    ]
+
 router.post('/players', (req, res) => {
 
     let name = req.body.name;
