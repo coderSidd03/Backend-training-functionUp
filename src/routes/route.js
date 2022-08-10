3679e0026b760e999523d66334f49903ced76d9c
soumya <97720811+coderSidd03@users.noreply.github.com>
2022-08-10 14:23:18 +0530

    assignment-post-api-players-details

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,53 +2,58 @@ const express = require('express');
 
 const router = express.Router();
 
+let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ],
+        },
+    ]
 
-//post api syntax
-//we have to use 
-// router.post
-
-router.post('/test-post', function(req,res) {
-    res.send([23,45,6])
-});
-
-//sending object
-router.post('/test-post-obj', function(req,res) {
-    res.send( { msg: 'hi', status: true } )
-});
 
 
-// if we want to send confidential or secured information
-// like username & password
-// we typically send this datas inside body
-// select post > body > raw > JSON (from dropdown)
-// then type Json data like key value pairs
-/*
-    {
-        "user": "sid"
-        "password": "1234"
+
+router.post('/players', (req, res) => {
+
+    let name = req.body.name;
+
+    for (var i in players) {
+        let player = players[i];
+        if (name == player.name) {
+            res.send(">> Player is already existed")
+        }
     }
-*/
 
-router.post('/test-post-body', function(req,res) {
-    let pwd = req.body.password
-    let id = req.body.user;
-    console.log(id, pwd)
-    
-    console.log(req.body)
-    res.send( { msg: 'hi', status: true } )
+    players.push(req.body)
+    res.send('>> player added')
+    console.log(players)
 });
 
 
 
-
-// take input in a post request and add it to an array and return the new array
-router.post('/test-post-4', function (req, res) {
-    let arr = [12 , 'functionUp']
-    let ele = req.body.element
-    arr.push(ele)
-    
-    res.send( {msg: 'dummy'} );
-})
 
 
 
