b64421e50f3665842959c70ee67a253c730f708b
soumya <97720811+coderSidd03@users.noreply.github.com>
2022-08-12 15:10:40 +0530

    mongo DB session 0

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://functionUp:cszjqDBOWDuVK5I9@cluster0.ge3cm6p.mongodb.net/sidd-01?retryWrites=true&w=majority", 
+    {
+        useNewUrlParser: true
+    }
+).then(() => { console.log("MongoDb is connected") })
+    .catch(err => console.log(err))
+
+
+
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
-
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,86 +1,41 @@
-const { query } = require('express');
 const express = require('express');
+const myHelper = require('../util/helper')
+const underscore = require('underscore')
 
 const router = express.Router();
 
-
-
-
-// ASSIGNMENT :- 1 <post-api> <player-data>
-
-router.post('/players', (req, res) => {
-
-    let name = req.body.name;
-    let player = players.find((element) => element.name == name)
-
-    (player) ? res.send(">> Player is already existed") 
-    :
-    players.push(req.body)
-    res.send(players);
-    
+router.get('/test-me', function (req, res) {
+    myHelper.printDate()
+    myHelper.getCurrentMonth()
+    myHelper.getCohortData()
+    let firstElement = underscore.first(['Sabiha', 'Akash', 'Pritesh'])
+    console.log('The first element received from underscope function is ' + firstElement)
+    res.send('My first ever api!')
 });
 
+router.get('/hello', function (req, res) {
 
+    res.send('Hello there!')
+});
 
-//problem : 2 <votingAge filter problem>
+router.get('/candidates', function (req, res) {
+    console.log('Query paramters for this request are ' + JSON.stringify(req.query))
+    let gender = req.query.gender
+    let state = req.query.state
+    let district = req.query.district
+    console.log('State is ' + state)
+    console.log('Gender is ' + gender)
+    console.log('District is ' + district)
+    let candidates = ['Akash', 'Suman']
+    res.send(candidates)
+})
 
-let persons = [
-    {
-        name: "PK",
-        age: 10,
-        votingStatus: false
-    },
-    {
-        name: "SK",
-        age: 20,
-        votingStatus: false
-    },
-    {
-        name: "AA",
-        age: 70,
-        votingStatus: false
-    },
-    {
-        name: "SC",
-        age: 5,
-        votingStatus: false
-    },
-    {
-        name: "HO",
-        age: 40,
-        votingStatus: false
-    }
-]
-
-router.post('/persons', (req, res) => {
-    let votingAge = req.query.age;
-    let ageResult = persons.filter((elem) => elem.age > votingAge)
-    for (let i in ageResult) { ageResult[i].votingStatus = true; }
-    res.send(ageResult)
+router.get('/candidates/:canidatesName', function (req, res) {
+    console.log('The request objects is ' + JSON.stringify(req.params))
+    console.log('Candidates name is ' + req.params.canidatesName)
+    res.send('Done')
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = router;
+// adding this comment for no reason
